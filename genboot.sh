a0b67302447f2894c4a6b2a6c9ffd88d6c41107e
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-02-16 21:21:59 -0200

    genboot.sh: Add more ramdisks
    From latests CWM builds in xda
    
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- genboot.sh
+++ genboot.sh
@@ -3,16 +3,27 @@
 # RAMDisk is from Xperia E1 Single
 # This a force boot.img, i can't make a live boot changer like in LG, because bootloader
 
-rm -rf boot-creator/boot.img
+rm -rf boot-creator/*.img
 
+bootcreator(){
 ./boot-creator/tool/mkqcdtbootimg \
     --kernel arch/arm/boot/zImage \
-    --ramdisk boot-creator/ramdisk/ramdisk.gz \
-    --dt_dir arch/arm/boot/ \
+    --ramdisk boot-creator/ramdisk/$VARIANT-ramdisk \
+    --dt_dir arch/arm/boot \
     --cmdline "`cat boot-creator/ramdisk/cmdline`" \
     --base 0x00000000 \
     --ramdisk_offset 0x2008000 \
     --kernel_offset 0x10000 \
     --tags_offset 0x1e08000 \
     --pagesize 2048 \
-    -o boot-creator/boot.img
+    -o boot-creator/$VARIANT-boot.img
+}
+
+VARIANT="single"
+bootcreator
+
+VARIANT="dual"
+bootcreator
+
+VARIANT="tv"
+bootcreator
